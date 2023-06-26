0e6e1d03495ab916a5fcbe5b12fbffd86206bf62
s2rgi0 <tlakolu@gmail.com>
2023-06-26 11:13:21 -0600

    Category COntroller Tests

--- src/test/java/guru/springframework/spring5webfluxrest/controller/CategoryControllerTest.java
+++ src/test/java/guru/springframework/spring5webfluxrest/controller/CategoryControllerTest.java
@@ -6,11 +6,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 
 public class CategoryControllerTest {
 
@@ -54,4 +56,19 @@ public class CategoryControllerTest {
                 .expectBody(Category.class);
 
     }
+
+    @Test
+    public void testCreateCategory(){
+        BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Category.builder().build()));
+        Mono<Category> catToSaveMono = Mono.just(Category.builder().description("Some Category").build());
+
+        webTestClient.post()
+                .uri("/api/v1/categories")
+                .body(catToSaveMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+
 }
